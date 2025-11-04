87bc019d5f06bd9527d1af15d73541932f24b13b
NguyenHoHongHai <haidc24v7x311@dttx.ctu.edu.vn>
2025-11-04 15:46:13 +0700

    Cai dat xu ly loi ApiError

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
+// --------------------
+// Xử lý lỗi 404 và lỗi chung
+// --------------------
+const ApiError = require("./app/api-error");
+
+// Xử lý lỗi khi không tìm thấy route
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi tập trung
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+module.exports = app;
