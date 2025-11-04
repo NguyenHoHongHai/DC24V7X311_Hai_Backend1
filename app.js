1451cd330ff1547c38e3f391155a9374d8f933e1
NguyenHoHongHai <haidc24v7x311@dttx.ctu.edu.vn>
2025-11-04 15:31:17 +0700

    Cap nhat Contact routes va controller

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
+
 // Route kiểm tra hoạt động
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to ContactBook application." });
